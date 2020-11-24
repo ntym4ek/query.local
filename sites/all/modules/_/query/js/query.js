45fe8a88d8c2da7e0e69332cb71aaf4420b02fcb
ntym4ek <mega_comp@mail.ru>
2020-11-24 09:50:04 +0300

    Выбор дат в форме, выделение ТО

--- sites/all/modules/_/query/js/query.js
+++ sites/all/modules/_/query/js/query.js
@@ -1,44 +1,70 @@
 (function ($) {
 
   var form_states = {};
+  var isMouseDown = false;
 
   Drupal.behaviors.query = {
     attach: function (context, settings) {
 
       // сохранить начальное состояние чекбоксов
-      $("form.produce-unit-form").each(function() {
-        $(this).find("button[id^=edit-submit]").hide();
-        var form_id = $(this).attr("id");
-        $(this).find("input[type=checkbox]").each(function() {
-          var input_id = $(this).attr("id");
-          var input_value = $(this).is(':checked');
-          if (form_states[form_id] == undefined) form_states[form_id] = {};
-          form_states[form_id][input_id] = input_value;
-        });
-      });
+      // $("form.query-form").each(function() {
+      //   $(this).find("button[id^=edit-submit]").hide();
+      //   var form_id = $(this).attr("id");
+      //   $(this).find("input[type=checkbox]").each(function() {
+      //     var input_id = $(this).attr("id");
+      //     var input_value = $(this).is(':checked');
+      //     if (form_states[form_id] == undefined) form_states[form_id] = {};
+      //     form_states[form_id][input_id] = input_value;
+      //   });
+      // });
 
       // в зависимости от наличия изменений чекбоксов вывести или спрятать кнопку Сохранить
-      $(".produce-unit-form input[type=checkbox]").click(function() {
-        var form = $(this).closest("form");
-        var is_changed = false;
-        var days_count = 0;
-        $(this).closest("form").find("input[type=checkbox]").each(function() {
-          var input_id = $(this).attr("id");
-          var input_val = $(this).is(':checked');
-          if (input_val != form_states[form.attr("id")][input_id]) {
-            is_changed = true;
-          }
-          if (input_val) days_count++;
-        });
-        if (is_changed) $(form).find("button[id^=edit-submit]").show();
-        else $(form).find("button[id^=edit-submit]").hide();
+      // $(".query-form input[type=checkbox]").click(function() {
+      //   var form = $(this).closest("form");
+      //   var is_changed = false;
+      //   var days_count = 0;
+      //   $(this).closest("form").find("input[type=checkbox]").each(function() {
+      //     var input_id = $(this).attr("id");
+      //     var input_val = $(this).is(':checked');
+      //     if (input_val != form_states[form.attr("id")][input_id]) {
+      //       is_changed = true;
+      //     }
+      //     if (input_val) days_count++;
+      //   });
+      //   if (is_changed) $(form).find("button[id^=edit-submit]").show();
+      //   else $(form).find("button[id^=edit-submit]").hide();
+      //
+      //   // todo пересчёт количества от выбранных дней
+      //   var amount_el = $(this).closest("form").find(".produce-unit-performance .amount");
+      //   var amount = amount_el.data("performance") * days_count;
+      //   amount_el.find("span").text(amount);
+      // });
 
-        // todo пересчёт количества от выбранных дней
-        var amount_el = $(this).closest("form").find(".produce-unit-performance .amount");
-        var amount = amount_el.data("performance") * days_count;
-        amount_el.find("span").text(amount);
+      // выделение нескольких чекбоксов проведением над ними -----------------------------------------------------------
+      $("body").mousedown(function() { isMouseDown = true });
+      $("body").mouseup(function() { isMouseDown = false });
+
+      $(".query-form input[type=checkbox]+span").mousedown(function(e) {
+        isMouseDown = true;
+        var input = $(this).closest('.form-type-checkbox').find('input');
+        checkCheck(input);
+      });
+      $(".query-form input[type=checkbox]+span").mouseover(function() {
+        var input = $(this).closest('.form-type-checkbox').find('input');
+        checkCheck(input)
+      });
+      $('.query-form input[type="checkbox"]').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
       });
 
+      function checkCheck(el) {
+        if (isMouseDown) {
+          var input_value = el.is(':checked');
+          el.prop("checked", !input_value);
+          console.log(!input_value);
+        }
+      }
 
     }
   };
