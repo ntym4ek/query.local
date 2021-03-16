a9770f9e53b2e0f46a7ba38bf4fe7a321ba0e8ce
ntym4ek <mega_comp@mail.ru>
2021-03-16 10:44:46 +0300

    Правка повторной отправки заявки

--- sites/all/modules/_/query/js/query.js
+++ sites/all/modules/_/query/js/query.js
@@ -1,44 +1,18 @@
 (function ($) {
 
-  var form_states = {};
   var isMouseDown = false;
 
   Drupal.behaviors.query = {
     attach: function (context, settings) {
 
-      // сохранить начальное состояние чекбоксов
-      // $("form.query-form").each(function() {
-      //   $(this).find("button[id^=edit-submit]").hide();
-      //   var form_id = $(this).attr("id");
-      //   $(this).find("input[type=checkbox]").each(function() {
-      //     var input_id = $(this).attr("id");
-      //     var input_value = $(this).is(':checked');
-      //     if (form_states[form_id] == undefined) form_states[form_id] = {};
-      //     form_states[form_id][input_id] = input_value;
-      //   });
-      // });
+      // форма создания Заявки, сделать кнопку сабмита неактивной после нажатия
+      $(".query-form [name=eck_submit]").on("click", function() {
+        // без таймаута не даст форме сработать событию сабмита
+        setTimeout(function () {
+          $(".query-form [name=eck_submit]").prop('disabled', true).text("Отправка...");
+        }, 0);
+      });
 
-      // в зависимости от наличия изменений чекбоксов вывести или спрятать кнопку Сохранить
-      // $(".query-form input[type=checkbox]").click(function() {
-      //   var form = $(this).closest("form");
-      //   var is_changed = false;
-      //   var days_count = 0;
-      //   $(this).closest("form").find("input[type=checkbox]").each(function() {
-      //     var input_id = $(this).attr("id");
-      //     var input_val = $(this).is(':checked');
-      //     if (input_val != form_states[form.attr("id")][input_id]) {
-      //       is_changed = true;
-      //     }
-      //     if (input_val) days_count++;
-      //   });
-      //   if (is_changed) $(form).find("button[id^=edit-submit]").show();
-      //   else $(form).find("button[id^=edit-submit]").hide();
-      //
-      //   // todo пересчёт количества от выбранных дней
-      //   var amount_el = $(this).closest("form").find(".produce-unit-performance .amount");
-      //   var amount = amount_el.data("performance") * days_count;
-      //   amount_el.find("span").text(amount);
-      // });
 
       // выделение нескольких чекбоксов проведением над ними -----------------------------------------------------------
       $("body").once(function() {
