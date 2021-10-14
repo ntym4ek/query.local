55049f41d7f6f3d62cfd2e14e094421d22258dd5
ntym4ek <mega_comp@mail.ru>
2021-10-14 16:20:02 +0300

    Модуль Склад

---
+++ sites/all/modules/_/warehouse/js/warehouse.js
@@ -0,0 +1,43 @@
+(function ($) {
+  Drupal.behaviors.warehouse = {
+    attach: function (context, settings) {
+
+      $(".warehouse-form .day").on("click", function() {
+        if ($(this).hasClass("active")) {
+          $(this).removeClass("active");
+        } else {
+          $(".warehouse-form .day").each(function() {
+            $(this).removeClass("active");
+          });
+
+          $(this).addClass("active");
+          updateTime();
+        }
+      });
+
+      $(".warehouse-form .half").on("click", function() {
+        if ($(this).hasClass("active")) {
+          $(this).removeClass("active");
+        } else {
+          if ($(this).hasClass("free")) {
+            $(".warehouse-form .half").each(function() {
+              $(this).removeClass("active");
+              $("input[name=time]").val(0);
+            });
+
+            // выделить свободное время и занести данные в форму
+            $(this).addClass("active");
+            updateTime();
+          }
+        }
+      });
+
+      function updateTime() {
+        var $date = $(".warehouse-form .day.active").data("date");
+        var $start_offset = $(".warehouse-form  .half.active").data("start-offset") * 60;
+
+        $("input[name=time]").val($date + $start_offset);
+      }
+    }
+  }
+})(jQuery);
