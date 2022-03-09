4e701f9375ab71a4ad58783c89dc38d02f0a4e2c
ntym4ek <mega_comp@mail.ru>
2022-03-09 14:00:29 +0300

    Таблица заявок

---
+++ sites/all/themes/prod/js/prod.js
@@ -0,0 +1,22 @@
+(function ($) {
+  Drupal.behaviors.prod = {
+    attach: function (context, settings) {
+
+
+      // куки в списке магазинов -----------------------------------------------
+      // $('.page-production-queries').once(function() {
+      //   $('a.arch').on('click', function () {
+      //     var id = $(this).data('id');
+      //     if ($(this).hasClass('visible')) {
+      //       $(this).removeClass('visible');
+      //       $('.arch-' + id).removeClass('visible');
+      //     } else {
+      //       $(this).addClass('visible');
+      //       $('.arch-' + id).addClass('visible');
+      //     }
+      //   });
+      // });
+
+    }
+  };
+})(jQuery);
