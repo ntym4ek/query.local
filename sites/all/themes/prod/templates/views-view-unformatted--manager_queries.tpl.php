11bcf73abaadaca28f194badfbf2f316073d422a
ntym4ek <mega_comp@mail.ru>
2022-03-21 09:46:16 +0300

    Правки

--- sites/all/themes/prod/templates/views-view-unformatted--manager_queries.tpl.php
+++ sites/all/themes/prod/templates/views-view-unformatted--manager_queries.tpl.php
@@ -6,25 +6,36 @@
  *
  * @ingroup views_templates
  */
+$nom = 0;
+$first = $last = false;
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<?php foreach ($rows as $id => $row): ?>
+<?php foreach ($rows as $index => $row): ?>
 
-  <?php if (isset($prefix_row_key) && $id == $prefix_row_key): ?>
-    <button class="btn btn-default btn-xs btn-archive" type="button" data-toggle="collapse" data-target="#collapse-<? print $id; ?>" aria-expanded="false" aria-controls="collapse-<? print $id; ?>">
-      Архив<?php print '';?>
-    </button>
-    <div class="collapse" id="collapse-<? print $id; ?>">
-  <?php endif; ?>
+  <?
+    if ($nom != $variables["view"]->result[$index]->field_data_field_nomenklatura_field_nomenklatura_target_id) {
+    $nom = $variables["view"]->result[$index]->field_data_field_nomenklatura_field_nomenklatura_target_id;
+    $first = true; $last = false;
+    $id = $variables["view"]->result[$index]->id;
+    } else {
+    $first = false;
+    }
+    if (empty($variables["view"]->result[$index+1]) || $nom != $variables["view"]->result[$index+1]->field_data_field_nomenklatura_field_nomenklatura_target_id) {
+    $last = true;
+    }
+  ?>
 
-  <div<?php if ($classes_array[$id]): ?> class="<?php print $classes_array[$id]; ?>"<?php endif; ?>>
+  <div<?php if ($classes_array[$index]): ?> class="<?php print $classes_array[$index]; ?>"<?php endif; ?>>
     <?php print $row; ?>
   </div>
 
-  <?php if (isset($suffix_row_key) && $id == $suffix_row_key): ?>
-    </div><?php print '';?>
+  <?php if ($first): ?>
+    <div class="collapse" id="collapse-<? print $id; ?>">
   <?php endif; ?>
 
+  <?php if ($last): ?>
+    </div>
+  <?php endif; ?>
 <?php endforeach; ?>
